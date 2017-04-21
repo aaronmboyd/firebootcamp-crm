6ae81f516c542ff648ccfa8a661f28a42056241c
Aaron Boyd <aaronmboyd@gmail.com>
2017-04-21 11:58:45 +1000

    Manual service

--- src/app/company/company-list/company-list.component.ts
+++ src/app/company/company-list/company-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CompanyService } from '../company.service';
+import { Company } from '../company';
 
 @Component({
   selector: 'fbc-company-list',
@@ -7,19 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CompanyListComponent implements OnInit {
     
-  companies: { id: number; name: string; email: string; phone: number; }[];
+  companies: Company[];
 
-  constructor() { }
+  // Using private keyword creates a class-level private variable and assign what is passed in
+  // Rather than creating a new private variable and assigning manually
+  constructor( private companyService : CompanyService){} 
 
   ngOnInit() {
     this.getCompanies();
   }
 
   getCompanies(){
-    return this.companies = [
-      {id:-1, name: 'company 1', email: 'email1', phone: 123},
-      {id:-2, name: 'company 2', email: 'email2', phone: 223},
-    ]
+    this.companyService.getCompanies()
+    .subscribe(
+      companies => this.companies = companies,
+      error => console.log('ERROR', error),
+      () => console.log('Completed')   );
   }
-
 }
--- src/app/company/company.service.ts
+++ src/app/company/company.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
+import { Company } from './company';
 
 @Injectable()
 export class CompanyService {
 
   constructor() { }
 
+  getCompanies() : Observable<Company[]>{
+    return Observable.of( [
+      {id:-1, name: 'company 1', email: 'email1', phone: 123},
+      {id:-2, name: 'company 2', email: 'email2', phone: 223},
+    ]);
+  }
 }
