77974f24508552cd7b70621b83476a5cb856be4c
Aaron Boyd <aaronmboyd@gmail.com>
2017-04-21 10:39:43 +1000

    JSON pipe company-list-component

--- src/app/company/company-list/company-list.component.ts
+++ src/app/company/company-list/company-list.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./company-list.component.scss']
 })
 export class CompanyListComponent implements OnInit {
+    
+  companies: { id: number; name: string; email: string; phone: number; }[];
 
   constructor() { }
 
   ngOnInit() {
+    this.getCompanies();
+  }
+
+  getCompanies(){
+    return this.companies = [
+      {id:-1, name: 'company 1', email: 'email1', phone: 123},
+      {id:-2, name: 'company 2', email: 'email2', phone: 223},
+    ]
   }
 
 }
